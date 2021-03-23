c90b7f56a08249058391ffd888fbecd7cae94f7f
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-03-23 11:21:44 +0100

    task 6

--- apps/customer-functional/src/integration/3-cart-page.spec.ts
+++ apps/customer-functional/src/integration/3-cart-page.spec.ts
@@ -133,6 +133,32 @@ describe(`The Cart page`, () => {
           .and('contain', '$0');
       });
 
+      it(`when an error occurs, user should be redirected to the error page with the cart contents kept`, () => {
+        cy.intercept('POST', '/api/order', { forceNetworkError: true }).as('orderRequest');
+
+        cy.get(`[data-test-id="place order"]`)
+          .should('be.visible')
+          .and('not.be.disabled')
+          .click();
+
+        cy.wait('@orderRequest')
+          .its('request.body').should('deep.include', {
+          address: 'Gotham, Crime Alley 32.',
+          paymentType: 'CARD'
+        });
+
+        cy.url().should('contain', '/error');
+
+        cy.get(`[data-test-id="error message"]`)
+          .should('be.visible')
+          .and('contain', 'Sorry, an unexpected error occurred. Please try again later.');
+
+        cy.get(`[data-test-id="cart button"]`)
+          .should('be.visible')
+          .and('have.css', 'opacity', '1')
+          .and('contain', '$26.90');
+      });
+
     });
   });
 });
