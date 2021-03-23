73e6f8d83f99184e57362c0cb7f3097fc40f536b
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-03-23 11:14:12 +0100

    task 1

--- apps/customer-functional/src/integration/1-pizza-list.spec.ts
+++ apps/customer-functional/src/integration/1-pizza-list.spec.ts
@@ -10,4 +10,19 @@ describe(`Pizza interceptors`, () => {
    * Write two tests that verifies that when a network error or an unexpected server error occurs, an error message is displayed to the user.
    */
 
+  describe(`when there is an empty pizza list response`, () => {
+    beforeEach(() => {
+      cy.intercept('GET', '/api/pizza/list', { body: [] }).as('emptyList');
+      cy.visit('/');
+      cy.wait('@emptyList');
+    });
+
+    it(`a message should be displayed`, () => {
+      cy.get(`[data-test-id="no delivery"]`)
+        .should('exist')
+        .and('be.visible')
+        .and('contain', 'Sorry, but we are not delivering pizzas at the moment.');
+    });
+  });
+
 });
