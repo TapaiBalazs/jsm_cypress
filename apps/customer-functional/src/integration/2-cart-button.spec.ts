2e259f55890e5d4bfbc6e8e61a8c7a3995f2aa94
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-03-23 11:19:34 +0100

    task 4

--- apps/customer-functional/src/integration/2-cart-button.spec.ts
+++ apps/customer-functional/src/integration/2-cart-button.spec.ts
@@ -8,15 +8,18 @@ describe(`The Cart button`, () => {
    */
 
   beforeEach(() => {
-
+    cy.intercept('GET', '/api/pizza/list', { fixture: 'pizzas.json' }).as('pizzas');
+    cy.intercept('GET', '/api/pizza/images/*.jpg', { fixture: 'pizza.jpg' }).as('pizzaImage');
+    cy.visit('/pizza');
+    cy.wait('@pizzas');
   });
 
-  xit(`clicking on the cart button navigates to the /cart page`, () => {
+  it(`clicking on the cart button navigates to the /cart page`, () => {
     cy.get(`[data-test-id="cart button"]`).should('be.visible').click();
     cy.url().should('contain', '/cart');
   });
 
-  xit(`the cart button should always be visible`, () => {
+  it(`the cart button should always be visible`, () => {
     // we also test if the price gets converted to dollar amounts
     cy.get(`[data-test-id="cart button"]`)
       .should('be.visible')
@@ -24,7 +27,7 @@ describe(`The Cart button`, () => {
       .and('contain', '$0');
   });
 
-  xit(`pizzas can be added to the cart which updates the css`, () => {
+  it(`pizzas can be added to the cart which updates the css`, () => {
     // we get the whole pizza row which has the data-test-id
     cy.get(`[data-test-id="Margherita"]`)
       .should('be.visible')
@@ -38,7 +41,7 @@ describe(`The Cart button`, () => {
       .and('contain', '$12.90');
   });
 
-  xit(`adding multiple pizzas sums up the order price`, () => {
+  it(`adding multiple pizzas sums up the order price`, () => {
     // we get the whole pizza row which has the data-test-id
     cy.get(`[data-test-id="Margherita"]`)
       .should('be.visible')
